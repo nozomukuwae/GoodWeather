69d6b7d0901562ab6b41218a448339da3a4a2793
Nozomu Kuwae <nozomukuwae@gmail.com>
2019-09-24 21:19:59 +0900

    Initial commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Nozomu Kuwae on 9/24/19.
+//  Copyright © 2019 Nozomu Kuwae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel! 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
